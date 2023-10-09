f5d445490c7d1a45194c935ef0c28cfed1183ea3
Nikolay <103648794+gilotin@users.noreply.github.com>
2023-10-09 12:50:50 +0300

    setup the project

---
+++ src/app/dbConfig/dbConfig.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try {
+        mongoose.connect(process.env.MONGO_URL!);
+        const connection = mongoose.connection;
+        connection.on("connected", () => {
+            console.log("MongoDB connected successfully!");
+        });
+        connection.on("error", (error) => {
+            console.log("MongoDB connection error. Please make sure MongoDB is running." + error);
+            process.exit();
+        });
+    } catch (error) {
+        console.log("Something is Wrong!");
+        console.log(error);
+    }
+}
