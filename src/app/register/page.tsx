9ba93b05f03f85d4419d9e06e19bb36e836967cd
Nikolay <103648794+gilotin@users.noreply.github.com>
2023-10-09 14:21:21 +0300

    creating Register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,63 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import styles from "../register/register.module.css";
+
 export default function Register() {
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+        username: "",
+        repeatPassword: "",
+    });
+
+    const onRegister = async () => {};
+
     return (
-        <>
-            <h1>register page</h1>
-        </>
+        <div className={styles.container}>
+            <form className={styles.form}>
+                <h1>register</h1>
+
+                <label className={styles.label} htmlFor="username">
+                    username
+                </label>
+                <input
+                    type="text"
+                    id="username"
+                    onChange={(e) => setUser({ ...user, username: e.target.value })}
+                    placeholder="username"
+                />
+                <label className={styles.label} htmlFor="email">
+                    email
+                </label>
+                <input
+                    type="text"
+                    id="email"
+                    onChange={(e) => setUser({ ...user, email: e.target.value })}
+                    placeholder="email"
+                />
+                <label className={styles.label} htmlFor="password">
+                    password
+                </label>
+                <input
+                    type="password"
+                    id="password"
+                    onChange={(e) => setUser({ ...user, password: e.target.value })}
+                    placeholder="password"
+                />
+                <label className={styles.label} htmlFor="repeatPassword">
+                    Repeat password
+                </label>
+                <input
+                    type="password"
+                    id="repeatPassword"
+                    onChange={(e) => setUser({ ...user, repeatPassword: e.target.value })}
+                    placeholder="password"
+                />
+                <button onClick={onRegister}>Register</button>
+            </form>
+        </div>
     );
 }
