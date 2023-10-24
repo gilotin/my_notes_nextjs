da79aa42df372c6e37d6e49ec28f9ea8c6d733a7
Nikolay <103648794+gilotin@users.noreply.github.com>
2023-10-25 00:54:00 +0300

    adjusted logout

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -7,7 +7,9 @@ export async function GET() {
             success: true,
         });
 
-        response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+        // response.cookies.delete("token");
+
+        response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) }); // alternative
 
         return response;
     } catch (error: any) {
