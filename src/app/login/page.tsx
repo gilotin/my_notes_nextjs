6edb9099b3ffb28dc8cf7458953cfb6df83d60cf
Nikolay <103648794+gilotin@users.noreply.github.com>
2023-10-24 22:53:09 +0300

    added login functionality

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,54 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import styles from "../login/login.module.css";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
+    const router = useRouter();
     const [user, setUser] = useState({
         email: "",
         password: "",
     });
 
-    const onLogin = async () => {};
+    const [loading, setLoading] = useState(false);
+
+    async function onLogin(e: any) {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            const response = await fetch("/api/users/login", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                },
+                body: JSON.stringify(user),
+            });
+            if (!response.ok) {
+                throw new Error(`Login failed ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            console.log("Login successful", data);
+            // you can implement toast latter
+            router.push("/profile");
+        } catch (error: any) {
+            console.log(":Login failed", error.message), toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    // useEffect(() => {
+
+    // }, [user]);
 
     return (
         <div className={styles.container}>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={(e) => onLogin(e)}>
                 <h1>Login</h1>
 
                 <label className={styles.label} htmlFor="email">
@@ -36,7 +69,7 @@ export default function LoginPage() {
                     onChange={(e) => setUser({ ...user, password: e.target.value })}
                     placeholder="password"
                 />
-                <button onClick={onLogin}>Register</button>
+                <button>Login</button>
                 <Link href="/register">Not registered ?</Link>
             </form>
         </div>
